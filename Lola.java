4862d6a6e010ee55e488ea12c6485189b33648b7
ValenLeon15 <an.leon@javeriana.edu.co>
2025-02-04 19:24:06 -0500

    Hola mundo Alejo

--- Lola.java
+++ Lola.java
@@ -2,5 +2,6 @@ public class Lola {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Hola Valen");
+        System.out.println("Hola Alejo");
     }
 }
